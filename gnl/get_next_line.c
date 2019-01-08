23d63ddb6d0e684ad10e708f1f1270026271c103
Orrin Argrave the defiant <oargrave@e2r1p1.21-school.ru>
2019-01-08 15:46:12 +0300

    GNL

---
+++ gnl/get_next_line.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oargrave <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/08 12:11:10 by oargrave          #+#    #+#             */
+/*   Updated: 2019/01/08 12:39:19 by oargrave         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+
+
+int	get_next_line(const int fd, char **line)
+{
+	char *buf;
+	char buuf;
+	int i;
+	int j = 0;
+	buf = NULL; // под вопросом
+	if (!(fd) || !(line) || BUFF_SIZE < 1)
+		return (-1);
+	if (!(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (-1);
+	while (( i = read (fd, buf, BUFF_SIZE)))
+	{
+		buf[BUFF_SIZE + 1] = '\0';
+		//line[0] = ft_strncpy(line[0],buf,BUFF_SIZE);
+		//buf[i] = 0;
+		printf ("HELLOO");
+		printf ("%s", buf);
+		return (1);
+	}
+	printf ("%s",buf);
+	
+	return (0);
+}
+
+
+
+
+int main (int argc, char **argv)
+{
+	char **line;
+	int flags = O_RDONLY;
+	int fd;
+	int i;
+	char buuf;
+
+	i = 0;
+	if (!(line = (char**)malloc(sizeof(char*) * 2)))
+		return (0);
+	while ( i != 2)
+	{
+		if (!(line[i] = (char*)malloc(sizeof(char) * 33)))
+			return (0); 
+			i++;
+	}
+	if (!(fd = open (argv[1],flags)))
+		return (0);
+	if ((i = get_next_line(fd, line)) == 1)
+	{
+		printf ("good");
+	} 
+	
+	close (fd);
+	i = 0;
+		printf ("%s",line[0]);
+		i++;
+	
+	return (0);
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oargrave <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/08 12:24:00 by oargrave          #+#    #+#             */
+/*   Updated: 2019/01/08 12:39:22 by oargrave         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+#include <fcntl.h>
+#include "libft/libft.h"
+#include <stdio.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
