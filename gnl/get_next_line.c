4252a4674995d7590f5b32cf9371ee833cf1302b
Orrin Argrave the defiant <oargrave@e2r1p1.21-school.ru>
2019-01-30 18:14:39 +0300

    gnl1

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,12 +6,41 @@
 /*   By: oargrave <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 12:11:10 by oargrave          #+#    #+#             */
-/*   Updated: 2019/01/10 18:07:46 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/01/30 18:13:44 by oargrave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+t_list *ft_get(t_list **file, int fd)
+{
+	t_list *tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if (tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	if (!tmp)
+		tmp = ft_lstnew("",fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+
+char *ft_get_next(char **line, t_list point)
+{
+	if (
+
+
+
+
+	return (*line);
+}
 
 
 int	get_next_line(const int fd, char **line)
@@ -19,24 +48,28 @@ int	get_next_line(const int fd, char **line)
 	int i = 0;
 	int j = 0;
     char *buf;
-	*line = NULL;
-	char *point;
+	char *poin;
+	static t_list *file;
 
-	point = NULL;
+	*line = NULL;
+	t_list *point;
+
 	if (!(fd) || !(line) || !(BUFF_SIZE))
 		return (-1);
 	if (!(buf = (char*)malloc (sizeof (char) * (BUFF_SIZE + 1))))
 		return (-1);
+	point = ft_get(&file,fd);
+//	point = ft_lstnew("", fd);
 	while (( i = read (fd, buf, BUFF_SIZE)))
 	{
 		buf[i] = '\0';
+		(*point).content = ft_strjoin((char *)point->content,buf);
+		if (ft_strchr((char *)point->content, '\n'))
+				break ;
 	}
-	printf ("%s\n",buf);
-	point  = ft_strjoin(point,buf);
-	free(buf);
-	line = &point;
-	printf ("%s\n %s\n",*line,buf); 
-	return (0);
+	*line  = point->content;
+	buf = NULL; 
+	return (1);
 }
 
 
@@ -56,10 +89,11 @@ int main (int argc, char **argv)
 	i = 0;
 	if ((i = get_next_line(fd, &line)) == 1)
 	{
-		printf ("good");
+		printf ("good\n");
 	} 
 //	printf ("%s",line[0]);
 	close (fd);
-	printf ("%s\n",line);
+	printf ("%s",line);
+	free(line);
 	return (0);
 }
