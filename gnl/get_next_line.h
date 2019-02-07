8c7a22a3b0c24407e192f4da849fc399796777b6
Orrin Argrave the defiant <oargrave@e2r14p2.21-school.ru>
2019-02-07 17:25:29 +0300

    gnl

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,17 +6,16 @@
 /*   By: oargrave <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 12:24:00 by oargrave          #+#    #+#             */
-/*   Updated: 2019/02/07 11:58:58 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/02/07 13:43:02 by oargrave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10
 
 # include <fcntl.h>
 # include "libft/libft.h"
-# include <stdio.h>
 
 int	get_next_line(int fd, char **line);
 
--- gnl/main.c
+++
@@ -1,9 +0,0 @@
-#include "get_next_line.h"
-
-int main(int argc, char **argv)
-{
-	int fd;
-	
-	fd = 5; 
-	return (0);
-}
