7c578d199e5cdf64d110b71817ea79aadefe3816
Denis-shl <denish13@live.ru>
2019-09-06 15:35:29 +0300

    new files

--- minishell/src/echo.c
+++ minishell/src/echo.c
@@ -6,7 +6,7 @@
 /*   By: oargrave <oargrave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 17:57:52 by oargrave          #+#    #+#             */
-/*   Updated: 2019/09/06 13:28:18 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/09/06 15:31:44 by oargrave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 void	ft_echo_dol(char **str)
 {
-	int index;
-	char *var_env;
-	char *tmp;
+	int		index;
+	char	*var_env;
+	char	*tmp;
 
 	index = 0;
 	var_env = str[1];
 	var_env++;
 	while (g_env[index] != NULL)
 	{
-		if ((tmp = ft_strnstr(g_env[index], var_env, ft_strlen(var_env))) != NULL)
+		if ((tmp = ft_strnstr(g_env[index], var_env,
+						ft_strlen(var_env))) != NULL)
 		{
 			tmp += ft_strlen(var_env) + 1;
-			ft_printf ("%s\n", tmp);
+			ft_printf("%s\n", tmp);
 			return ;
 		}
 		index++;
 	}
-	ft_printf ("%s\n", str[1]);
-} 
+	ft_printf("%s\n", str[1]);
+}
 
 void	ft_echo(char **str)
 {
@@ -40,7 +41,6 @@ void	ft_echo(char **str)
 
 	index = 1;
 	del_commas(str);
-	
 	if (str[1] != NULL && ft_strcmp(str[1], NOTNEWLINE) == 0)
 		return ;
 	if (str[1] != NULL && str[1][0] == '$')
